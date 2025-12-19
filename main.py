b9d8f392e4e94db8ac24bac4088433dc393e7e56
BlueLaysYumYum <223233135+BlueLaysYumYum@users.noreply.github.com>
2025-12-19 18:23:26 +0530

    integrated it with the web server

---
+++ main.py
@@ -0,0 +1,122 @@
+import tweepy
+import pandas as pd
+import joblib
+from datetime import datetime,timezone
+
+
+
+client = tweepy.Client(
+    bearer_token="",
+    wait_on_rate_limit=True
+)
+
+username=input("enter username: ").strip()
+
+user = client.get_user(
+    username=username,
+    user_fields=[
+        "created_at",
+        "public_metrics",
+        "verified",
+        "description",
+        "profile_image_url",
+        "protected",
+        "location",
+        "profile_banner_url"
+    ]
+    )
+user_data = user.data
+
+if user_data is None:
+    print("User not found")
+    exit()
+
+
+
+#user profile photo
+profile_link=user_data.profile_image_url
+is_defaults=1
+link="https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
+if "default_profile" in profile_link:
+    is_defaults=1
+else:
+    is_defaults=0
+
+#account age
+today=datetime.now(timezone.utc)
+account_age=user_data.created_at
+account_age_days=(today-account_age).days
+
+#posts count
+posts=user_data.public_metrics["tweet_count"]
+
+#bio length
+if user_data.description:
+    bio_length=len(user_data.description)
+else:
+    bio_length=0
+
+
+#follow ratio
+followers=user_data.public_metrics["followers_count"]
+following=user_data.public_metrics["following_count"]
+if following==0:
+    follower_following_ratio=followers
+else:
+    follower_following_ratio=followers/following
+
+
+#verified
+verification=user_data.verified
+verified=0
+if verification==True:
+    verified=1
+
+#location
+location=user_data.location
+
+#profile background
+profile_background_url=user_data.profile_banner_url
+
+print("Username: @",username.strip())
+print("Profile:", is_defaults)
+print("followers:",followers)
+print("following:",following)
+print("follower ratio:",follower_following_ratio)
+print("Post count:",posts)
+print("account days created:",account_age_days)
+print("bio length:",bio_length)
+print("verified:",verified)
+print("location:",location)
+print("profile bg link:",profile_background_url)
+#Integration
+
+model_data = joblib.load("./bot_detector.pkl")
+model = model_data['model']
+features_list = model_data['features']
+print(features_list)
+user_value = pd.DataFrame(
+    [{"name":username.strip(),
+      "default_profile_image":is_defaults,
+      "followers_count":followers,
+      "friends_count":following,
+      "post_count":posts,
+      "account_age_days":account_age_days,
+      "bio_length":bio_length,
+      "verified":verified,
+      "profile_image_url":profile_link,
+      "location":location,
+      'profile_background_url':profile_background_url
+      }]
+)
+print(user_value)
+def data_unloading(data):
+    data["follower_to_following"] = data['followers_count'] / (data['friends_count'] + 1)
+    data['has_location'] = data['location'].notnull().astype(int)
+    data['profile_use_background_image'] = (data['profile_background_url']).astype(int)
+    data["post_frequency"] = data['post_count'] / (data['account_age_days'] + 1)
+    X_param = data[features_list]
+    return X_param
+input_data = data_unloading(user_value)
+prediction = model.predict(input_data)
+print(prediction)
--- ml.py
+++ ml.py
@@ -2,36 +2,41 @@
 import numpy as np
 import joblib
 import pandas as pd
-from xgboost import XGBRegressor
+from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 features_list = ["followers_count", "friends_count", "follower_to_following", "post_count", "has_location",
                      "default_profile_image", "profile_use_background_image", "verified", 'bio_length',
-                     "account_age_days"]
-#func for division was messing up at later parts
-# def data_unloading(data):
-#     data['bio_length'] = data['description'].str.len()
-#     data["created_at"] = pd.to_datetime(data["created_at"])
-#     data['account_age_days'] = (data['created_at'].max() - data['created_at']).dt.days
-#     data["follower_to_following"] = data['followers_count'] / (data['friends_count'] + 1)
-#     data['has_location'] = data['location'].notnull().astype(int)
-#
-#     X = bot_data[features_list]
-#     return X
-#csv reading and division
+                     "account_age_days","post_frequency"]
+#TODO add function
+# func for division was messing up at later parts
+def data_unloading(data):
+    data['bio_length'] = data['description'].str.len()
+    data["created_at"] = pd.to_datetime(data["created_at"])
+    data['account_age_days'] = (pd.to_datetime('today') - data['created_at']).dt.days
+    data["follower_to_following"] = data['followers_count'] / (data['friends_count'] + 1)
+    data['has_location'] = data['location'].notnull().astype(int)
+    data["post_frequency"] = data['post_count'] / (data['account_age_days'] + 1)
+    X_param = data[features_list]
+    return X_param
+# csv reading and division
+#TODO add tweets frequency
+
+# now = pd.to_datetime('today')
 bot_data = pd.read_csv("./twitter_profiles.csv")
-bot_data['bio_length']=bot_data['description'].str.len()
-bot_data["created_at"]=pd.to_datetime(bot_data["created_at"])
-bot_data['account_age_days'] = (bot_data['created_at'].max() - bot_data['created_at']).dt.days
-bot_data["follower_to_following"]=bot_data['followers_count']/(bot_data['friends_count']+1)
-bot_data['has_location'] = bot_data['location'].notnull().astype(int)
-features_list=["followers_count","friends_count","follower_to_following","post_count","has_location","default_profile_image","profile_use_background_image","verified",'bio_length',"account_age_days"]
-X = bot_data[features_list]
+# bot_data['bio_length']=bot_data['description'].str.len()
+# bot_data["created_at"]=pd.to_datetime(bot_data["created_at"])
+# bot_data['account_age_days'] = (pd.to_datetime('today') - bot_data['created_at']).dt.days
+# bot_data["follower_to_following"]=bot_data['followers_count']/(bot_data['friends_count']+1)
+# bot_data['has_location'] = bot_data['location'].notnull().astype(int)
+# features_list=["followers_count","friends_count","follower_to_following","post_count","has_location","default_profile_image","profile_use_background_image","verified",'bio_length',"account_age_days"]
+# X = bot_data[features_list]
+X = data_unloading(bot_data)
 y=bot_data["label"]
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
 
 #XGB regression
-XGB_model = XGBRegressor(n_estimators=1000, random_state=0,early_stopping_rounds=10)
+XGB_model = XGBClassifier(n_estimators=1000, random_state=0,early_stopping_rounds=10)
 XGB_model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=False)
 predictions = XGB_model.predict(X_test)
 scores = mean_absolute_error(y_test,predictions)
@@ -39,38 +44,31 @@ print(scores)
 
 #Custom input to make sure it is working with split
 
-custom_val = pd.DataFrame(
-    [{"name": ";saonidoa", "screen_name": "fojknedmsla", "followers_count": 43249, "friends_count": 23291,
-      "post_count": 21312,
-      "lang": "en", "location": "", "default_profile_image": 1, "profile_use_background_image": 0, "verified": 0,
-      "description": "", "created_at": "2025-04-18"}])
-custom_val['bio_length']=custom_val['description'].str.len()
-custom_val["created_at"]=pd.to_datetime(custom_val["created_at"])
-custom_val['account_age_days'] = (custom_val['created_at'].max() - custom_val['created_at']).dt.days
-custom_val["follower_to_following"]=custom_val['followers_count']/(custom_val['friends_count']+1)
-custom_val['has_location'] = custom_val['location'].notnull().astype(int)
-# X_check= data_unloading(custom_val)
-
-
-X_check = custom_val[features_list]
-print(X_check)
-
-#forest model to compare
-
-from sklearn.ensemble import RandomForestRegressor
-forest= RandomForestRegressor(random_state=1,)
-forest.fit(X,y)
-predictions_forest = forest.predict(X_check)
-prediction_main = XGB_model.predict(X_check)
-print(predictions_forest)
-print(prediction_main)
-if prediction_main[0] > 0.5:
-    print("Bot Detected")
-else:
-    print("Human Detected")
-
-#saving for web below code is ai
-
+# custom_val = pd.DataFrame(
+#     [{"name": ";saonidoa", "screen_name": "fojknedmsla", "followers_count": 113, "friends_count": 39,
+#       "post_count": 266,
+#       "lang": "en", "location": "", "default_profile_image": 0, "profile_use_background_image": 1, "verified": 0,
+#       "description": "stay away from bird", "created_at": "2025-04-18"}])
+# custom_val['bio_length']=custom_val['description'].str.len()
+# custom_val["created_at"]=pd.to_datetime(custom_val["created_at"])
+# print(custom_val['created_at'].max())
+# custom_val['account_age_days'] = (pd.to_datetime('today') - custom_val['created_at']).dt.days
+# custom_val["follower_to_following"]=custom_val['followers_count']/(custom_val['friends_count']+1)
+# custom_val['has_location'] = custom_val['location'].notnull().astype(int)
+# # X_check= data_unloading(custom_val)
+#
+#
+# X_check = custom_val[features_list]
+# print(X_check)
+# prediction_main = XGB_model.predict(X_check)
+# print(prediction_main)
+# # if prediction_main[0] > 0.5:
+# #     print("Bot Detected")
+# # else:
+# #     print("Human Detected")
+#
+# #saving for web below code is ai
+#
 model_data = {
     'model': XGB_model,
     'features': features_list,
